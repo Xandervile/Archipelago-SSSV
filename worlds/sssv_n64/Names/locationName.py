1447a46f2a8c7757fe3f5142c46cce8fcdcb9f64
Xander <121578706+Xandervile@users.noreply.github.com>
2026-05-23 17:32:10 +0100

    feat: added item locations (end of levels and tasks)

---
+++ worlds/sssv_n64/Names/locationName.py
@@ -0,0 +1,160 @@
+MENU_STARTING_LEVEL             = "Starting Level"
+
+#Europe
+#Smashing Start
+SMASHING_START_FINISH           = "Smashing Start Finish"
+SMASHING_START_TASK_1           = "Smashing Start Task 1"
+SMASHING_START_TASK_2           = "Smashing Start Task 2"
+
+#Have a Nice Day
+HAVE_A_NICE_DAY_FINISH          = "Have a Nice Day Finish"
+HAVE_A_NICE_DAY_TASK_1          = "Have a Nice Day Task 1"
+HAVE_A_NICE_DAY_TASK_2          = "Have a Nice Day Task 2"
+
+#Honeymoon Lagoon
+HONEYMOON_LAGOON_FINISH         = "Honeymoon Lagoon Finish"
+HONEYMOON_LAGOON_TASK_1         = "Honeymoon Lagoon Task 1"
+HONEYMOON_LAGOON_TASK_2         = "Honeymoon Lagoon Task 2"
+HONEYMOON_LAGOON_TASK_3         = "Honeymoon Lagoon Task 3"
+
+#The Battery Farm
+THE_BATTERY_FARM_FINISH         = "The Battery Farm Finish"
+THE_BATTERY_FARM_TASK_1         = "The Battery Farm Task 1"
+THE_BATTERY_FARM_TASK_2         = "The Battery Farm Task 2"
+THE_BATTERY_FARM_TASK_3         = "The Battery Farm Task 3"
+
+#The Engine Room
+THE_ENGINE_ROOM_FINISH          = "The Engine Room Finish"
+THE_ENGINE_ROOM_TASK_1          = "The Engine Room Task 1"
+THE_ENGINE_ROOM_TASK_2          = "The Engine Room Task 2"
+THE_ENGINE_ROOM_TASK_3          = "The Engine Room Task 3"
+
+#Fat Bear Mountain
+FAT_BEAR_MOUNTAIN_FINISH        = "Fat Bear Mountain Finish"
+FAT_BEAR_MOUNTAIN_TASK_1        = "Fat Bear Mountain Task 1"
+FAT_BEAR_MOUNTAIN_TASK_2        = "Fat Bear Mountain Task 2"
+
+#Rocky Hard Place
+ROCKY_HARD_PLACE_FINISH         = "Rocky Hard Place Finish"
+ROCKY_HARD_PLACE_TASK_1         = "Rocky Hard Place Task 1"
+ROCKY_HARD_PLACE_TASK_2         = "Rocky Hard Place Task 2"
+
+#Stinky Sewers
+STINKY_SEWERS_FINISH            = "Stinky Sewers Finish"
+STINKY_SEWERS_TASK_1            = "Stinky Sewers Task 1"
+STINKY_SEWERS_TASK_2            = "Stinky Sewers Task 2"
+STINKY_SEWERS_TASK_3            = "Stinky Sewers Task 3"
+
+#Rat-o-Matic
+RAT_O_MATIC_FINISH              = "Rat-o-Matic Finish"
+RAT_O_MATIC_TASK_1              = "Rat-o-Matic Task 1"
+RAT_O_MATIC_TASK_2              = "Rat-o-Matic Task 2"
+
+#Give a Dog a Bonus
+GIVE_A_DOG_A_BONUS_FINISH       = "Give a Dog a Bonus Finish"
+
+#Arctic
+#Snow Joke
+SNOW_JOKE_FINISH                = "Snow Joke Finish"
+SNOW_JOKE_TASK_1                = "Snow Joke Task 1"
+SNOW_JOKE_TASK_2                = "Snow Joke Task 2"
+SNOW_JOKE_TASK_3                = "Snow Joke Task 3"
+
+#Ice n Easy Does It
+ICE_N_EASY_DOES_IT_FINISH       = "Ice 'n' Easy Does It Finish"
+ICE_N_EASY_DOES_IT_TASK_1       = "Ice 'n' Easy Does It Task 1"
+
+#Penguin Playpen
+PENGUIN_PLAYPEN_FINISH          = "Penguin Playpen Finish"
+PENGUIN_PLAYPEN_TASK_1          = "Penguin Playpen Task 1"
+PENGUIN_PLAYPEN_TASK_2          = "Penguin Playpen Task 2"
+
+#Pinball Blizzard
+PINBALL_BLIZZARD_FINISH         = "Pinball Blizzard Finish"
+PINBALL_BLIZZARD_TASK_1         = "Pinball Blizzard Task 1"
+PINBALL_BLIZZARD_TASK_2         = "Pinball Blizzard Task 2"
+
+#Hoppa Choppa
+HOPPA_CHOPPA_FINISH             = "Hoppa Choppa Finish"
+HOPPA_CHOPPA_TASK_1             = "Hoppa Choppa Task 1"
+HOPPA_CHOPPA_TASK_2             = "Hoppa Choppa Task 2"
+
+#Something Fishy
+SOMETHING_FISHY_FINISH          = "Something Fishy Finish"
+SOMETHING_FISHY_TASK_1          = "Something Fishy Task 1"
+SOMETHING_FISHY_TASK_2          = "Something Fishy Task 2"
+
+#Walrace 64
+WALRACE_64_FINISH               = "Walrace 64 Finish"
+
+#Jungle
+#Jungle Japes
+JUNGLE_JAPES_FINISH             = "Jungle Japes Finish"
+JUNGLE_JAPES_TASK_1             = "Jungle Japes Task 1"
+JUNGLE_JAPES_TASK_2             = "Jungle Japes Task 2"
+
+#Jungle Doldrums
+JUNGLE_DOLDRUMS_FINISH          = "Jungle Doldrums Finish"
+JUNGLE_DOLDRUMS_TASK_1          = "Jungle Doldrums Task 1"
+JUNGLE_DOLDRUMS_TASK_2          = "Jungle Doldrums Task 2"
+
+#Swamp of Eternal Stench
+SWAMP_OF_ETERNAL_STENCH_FINISH  = "Swamp of Eternal Stench Finish"
+SWAMP_OF_ETERNAL_STENCH_TASK_1  = "Swamp of Eternal Stench Task 1"
+SWAMP_OF_ETERNAL_STENCH_TASK_2  = "Swamp of Eternal Stench Task 2"
+SWAMP_OF_ETERNAL_STENCH_TASK_3  = "Swamp of Eternal Stench Task 3"
+
+#Weight For It
+WEIGHT_FOR_IT_FINISH            = "Weight For It Finish"
+WEIGHT_FOR_IT_TASK_1            = "Weight For It Task 1"
+WEIGHT_FOR_IT_TASK_2            = "Weight For It Task 2"
+WEIGHT_FOR_IT_TASK_3            = "Weight For It Task 3"
+
+#Jungle Jumps
+JUNGLE_JUMPS_FINISH             = "Jungle Jumps Finish"
+JUNGLE_JUMPS_TASK_1             = "Jungle Jumps Task 1"
+JUNGLE_JUMPS_TASK_2             = "Jungle Jumps Task 2"
+JUNGLE_JUMPS_TASK_3             = "Jungle Jumps Task 3"
+
+#Evo's Escape
+EVOS_ESCAPE_Finish              = "Evo's Escape Finish"
+
+#Desert
+#Fun in the Sun
+FUN_IN_THE_SUN_FINISH           = "Fun in the Sun Finish"
+FUN_IN_THE_SUN_TASK_1           = "Fun in the Sun Task 1"
+FUN_IN_THE_SUN_TASK_2           = "Fun in the Sun Task 2"
+
+#Hot Cross Buns
+HOT_CROSS_BUNS_FINISH           = "Hot Cross Buns Finish"
+HOT_CROSS_BUNS_TASK_1           = "Hot Cross Buns Task 1"
+HOT_CROSS_BUNS_TASK_2           = "Hot Cross Buns Task 2"
+HOT_CROSS_BUNS_TASK_3           = "Hot Cross Buns Task 3"
+
+#Sting in the Tail
+STING_IN_THE_TAIL_FINISH        = "Sting in the Tail Finish"
+STING_IN_THE_TAIL_TASK_1        = "Sting in the Tail Task 1"
+STING_IN_THE_TAIL_TASK_2        = "Sting in the Tail Task 2"
+
+#Borassic Park
+BORASSIC_PARK_FINISH            = "Borassic Park Finish"
+BORASSIC_PARK_TASK_1            = "Borassic Park Task 1"
+BORASSIC_PARK_TASK_2            = "Borassic Park Task 2"
+
+#Whirlwind Tour
+WHIRLWIND_TOUR_FINISH           = "Whirlwind Tour Finish"
+WHIRLWIND_TOUR_TASK_1           = "Whirlwind Tour Task 1"
+WHIRLWIND_TOUR_TASK_2           = "Whirlwind Tour Task 2"
+WHIRLWIND_TOUR_TASK_3           = "Whirlwind Tour Task 3"
+
+#Shifting Sands
+SHIFTING_SANDS_FINISH           = "Shifting Sands Finish"
+SHIFTING_SANDS_TASK_1           = "Shifting Sands Task 1"
+SHIFTING_SANDS_TASK_2           = "Shifting Sands Task 2"
+
+#Punch Up Pyramid
+PUNCH_UP_PYRAMID_FINISH         = "Punch Up Pyramid Finish"
+
+#End
+#Big Celebration Parade
+BIG_CELEBRATION_PARADE_FINISH   = "Big Celebration Parade Finish"
